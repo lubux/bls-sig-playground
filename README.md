76d6ef0d17498894413fbd3a7ea73d06be9ae3fc
Lukas Burkhalter <lukas.burkhalter@ringwil.ch>
2020-01-31 17:11:59 +0100

    fixed build

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# bls-sig-playground
+# BLS-Signature Playground
+
+[![Build status](https://travis-ci.org/lubux/bls-sig-playground.svg?branch=master)](https://travis-ci.org/lubux/bls-sig-playground)
--- install-dep-ubuntu.sh
+++ install-dep-ubuntu.sh
@@ -3,3 +3,4 @@
 sudo apt-get update
 sudo apt-get install -y automake cmake build-essential libprocps-dev libtool\
                         pkg-config yasm texinfo autoconf flex bison
+sudo apt-install gcc-7 g++-7
