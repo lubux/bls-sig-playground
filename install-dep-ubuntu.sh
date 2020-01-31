b2bc3b248dbd1122130c7cf0ba1708e254a6847c
Lukas Burkhalter <lukas.burkhalter@ringwil.ch>
2020-01-31 17:14:21 +0100

    fix

--- install-dep-ubuntu.sh
+++ install-dep-ubuntu.sh
@@ -3,4 +3,5 @@
 sudo apt-get update
 sudo apt-get install -y automake cmake build-essential libprocps-dev libtool\
                         pkg-config yasm texinfo autoconf flex bison
-sudo apt-install gcc-7 g++-7
+
+sudo apt-get install gcc-7 g++-7
