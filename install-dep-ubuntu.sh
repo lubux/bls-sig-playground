e7052056aa671e956186ee702fa6161063b19071
Lukas Burkhalter <lukas.burkhalter@ringwil.ch>
2020-01-31 17:20:49 +0100

    gcc 7 travis

--- install-dep-ubuntu.sh
+++ install-dep-ubuntu.sh
@@ -3,5 +3,3 @@
 sudo apt-get update
 sudo apt-get install -y automake cmake build-essential libprocps-dev libtool\
                         pkg-config yasm texinfo autoconf flex bison
-
-sudo apt-get install gcc-7 g++-7
